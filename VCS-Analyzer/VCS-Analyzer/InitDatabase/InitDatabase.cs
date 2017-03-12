16d55fc04ba881606062d855cd209aeb793ed7c1
shehan peruma <shehan.peruma@gmail.com>
2017-03-12 17:38:56 -0400

    updated

---
+++ VCS-Analyzer/VCS-Analyzer/InitDatabase/InitDatabase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VCS_Analyzer.Services;
+
+namespace VCS_Analyzer.InitDatabase
+{
+    class InitDatabase : IProcess
+    {
+        public event EventHandler<NotificationArgs> NotificationIssued;
+
+        public void StartProcess()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
